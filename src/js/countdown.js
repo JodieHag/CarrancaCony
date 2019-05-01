309b2ed94011e55d67e5e344d1890ca98009e5bc
JodieHag <info@jodiehag.com>
2019-05-01 13:40:25 +0200

    production version

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,18 +1,18 @@
 const x = setInterval(() => {
-  let countDownDate = $('.countdown').data('date')
+  let countDownDate = $('.countdown').data('date');
 
-  countDownDate = new Date(countDownDate).getTime()
+  countDownDate = new Date(countDownDate).getTime();
 
   // Get todays date and time
-  let now = new Date().getTime()
+  let now = new Date().getTime();
   // Find the distance between now and the count down date
-  let distance = countDownDate - now
+  let distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
   $('.dday').html(days)
